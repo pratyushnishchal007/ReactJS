a68daee1f55435999e1e8c62167efe6b025357e5
Pratyush <74895862+pratyushnishchal007@users.noreply.github.com>
2021-07-03 17:49:19 +0530

    Add files via upload

---
+++ Challenge 1/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+<>
+  <h1>Netflix List</h1>
+  <p>List of 5 Best Series</p>
+  <ol>
+    <li>Dark</li>
+    <li>Extra Curricular</li>
+    <li>My holo love</li>
+    <li>My first-2 love</li>
+    <li>Mr Robot</li> 
+  </ol> 
+
+
+</>
+,document.getElementById("root")
+
+
+);
