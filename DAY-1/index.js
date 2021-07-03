0f0cce2b0386cfaf1f588be3ecb8787a3d30f58e
Pratyush <74895862+pratyushnishchal007@users.noreply.github.com>
2021-07-03 19:12:12 +0530

    Update index.js

--- DAY-1/index.js
+++ DAY-1/index.js
@@ -28,17 +28,33 @@ import ReactDOM from 'react-dom';
 
 // TEMPLATE LITERALS
 
-const fname="pratyush";
-const lname="nishchal";
+// const fname="pratyush";
+// const lname="nishchal";
 
-ReactDOM.render(
-    <>
-            <h1>{`My name is ${fname} ${lname}`}</h1>
-            <p>My lucky number is {5+6}</p>
-    </>    
-    ,document.getElementById("root"));
+// ReactDOM.render(
+//     <>
+//             <h1>{`My name is ${fname} ${lname}`}</h1>
+//             <p>My lucky number is {5+6}</p>
+//     </>    
+//     ,document.getElementById("root"));
 
+// const name="pratyush";
+// const img1="https://picsum.photos/200/300";
+// const img2="https://picsum.photos/300/300";
+// const img3="https://picsum.photos/400/300";
+// const href="https://www.google.com/search?q=google+drive&oq=goo&aqs=chrome.2.69i65j69i57j69i59l2j69i60l4.2083j0j7&sourceid=chrome&ie=UTF-8";
+// ReactDOM.render( 
+//     <>
+//     <h1 contentEditable="true">My name is {name}</h1>
+//     <img src={img1} alt="photos" />
+//     <img src={img2} alt="photos" />
+//     <a href={href} target="_blank">
+//     <img src={img3} alt="photos" />
+//     </a>
+// </>
+// ,document.getElementById("root")
 
+// );
 
 // ReactDOM.render(
 //     React.createElement("h1",null,"Pratyush"),document.getElementById("root")
