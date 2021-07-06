b81dec94d87f128afc05eb0e9723a5c7028ef3c1
Pratyush <74895862+pratyushnishchal007@users.noreply.github.com>
2021-07-06 16:24:39 +0530

    Add files via upload

---
+++ Props/Sdata.jsx
@@ -0,0 +1,36 @@
+const  Sdata=[ 
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQImTS8UM27W6bdn6baZzpIQp_btOS0UO2FBQ&usqp=CAU",
+    title:"A Netflix Original Series",
+    sname:"DARK",
+    link:"https://www.netflix.com/in/title/81107576",
+},
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQImTS8UM27W6bdn6baZzpIQp_btOS0UO2FBQ&usqp=CAU",
+    title:"A Netflix Original Series",
+    sname:"extra curricular", 
+    link:"https://www.netflix.com/in/title/81107576",
+}, 
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQImTS8UM27W6bdn6baZzpIQp_btOS0UO2FBQ&usqp=CAU",
+    title:"A Netflix Original Series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/81107576",
+},
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQImTS8UM27W6bdn6baZzpIQp_btOS0UO2FBQ&usqp=CAU",
+    title:"A Netflix Original Series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/81107576",
+},
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQImTS8UM27W6bdn6baZzpIQp_btOS0UO2FBQ&usqp=CAU",
+    title:"A Netflix Original Series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/81107576",
+}
+
+
+];
+
+export default Sdata;
---
+++ Props/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card.jsx';
+import "./index.css";
+import Sdata from './Sdata'
+
+ReactDOM.render(
+<>
+<h1 className="heading_style">List of Top 5 Netflix Series in 2020</h1>
+
+<Card  
+    imgsrc={Sdata[0].imgsrc}
+    title={Sdata[0].title}
+    sname={Sdata[0].sname}
+    link={Sdata[0].link}
+/>
+<Card  
+    imgsrc={Sdata[1].imgsrc}
+    title={Sdata[1].title}
+    sname={Sdata[1].sname}
+    link={Sdata[1].link}
+/>
+<Card  
+    imgsrc={Sdata[2].imgsrc}
+    title={Sdata[2].title}
+    sname={Sdata[2].sname}
+    link={Sdata[2].link}
+/>
+<Card  
+    imgsrc={Sdata[3].imgsrc}
+    title={Sdata[3].title}
+    sname={Sdata[3].sname}
+    link={Sdata[3].link}
+/>
+<Card  
+    imgsrc={Sdata[4].imgsrc}
+    title={Sdata[4].title}
+    sname={Sdata[4].sname}
+    link={Sdata[4].link}
+/>
+
+</>
+  ,document.getElementById('root'));
